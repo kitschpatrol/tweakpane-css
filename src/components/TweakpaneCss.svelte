1f333592a6bcc0e2b0fa6e4eef310316af5f1d2c
Eric Mika <eric@ericmika.com>
2024-05-18 03:59:00 -0400

    Add options, including automatic folder organization. Add option and folder state persistence.

--- src/components/TweakpaneCss.svelte
+++ src/components/TweakpaneCss.svelte
@@ -1,12 +1,13 @@
 <script context="module" lang="ts">
-	function getUnits(value: string): string {
+	function getUnits(value: string): string | undefined {
 		// Don't get confused by hex colors or complex expressions
 		if (Number.isNaN(Number.parseFloat(value))) return ''
 		const match = /^(-?[\d.]+)\s?([%a-z]*)$/i.exec(value)
-		return match?.[2] ?? ''
+		return match?.[2]
 	}
 
 	export function preload(): void {
+		// Apply any modified styles
 		if (typeof localStorage !== 'undefined') {
 			const cssVariables = localStorage.getItem('css')
 			if (cssVariables) {
@@ -27,22 +28,82 @@
 </script>
 
 <script lang="ts">
-	import { onMount } from 'svelte'
+	import { onMount, tick } from 'svelte'
 	import { persisted } from 'svelte-local-storage-store'
+	import { Button } from 'svelte-tweakpane-ui'
+	import AutoObject from 'svelte-tweakpane-ui/AutoObject.svelte'
 	import AutoValue from 'svelte-tweakpane-ui/AutoValue.svelte'
 	import ButtonGrid from 'svelte-tweakpane-ui/ButtonGrid.svelte'
 	import type { ButtonGridClickEvent } from 'svelte-tweakpane-ui/ButtonGrid.svelte'
+	import Folder from 'svelte-tweakpane-ui/Folder.svelte'
 	import Pane from 'svelte-tweakpane-ui/Pane.svelte'
 	import Separator from 'svelte-tweakpane-ui/Separator.svelte'
-	import type { Writable } from 'svelte/store'
+	import { type Writable } from 'svelte/store'
 
+	// Set up stores for local persistence
+	let cssVariableStore: Writable<Record<string, number | string>>
+	let optionsStore: Writable<Options>
+	let expandedStateStore: Writable<ExpandedState>
+
+	type Options = {
+		autoFolders?: boolean
+		includeCalculated?: boolean
+		prettyNames?: boolean
+		showUnits?: boolean
+		sortNames?: boolean
+	}
+
+	type FolderPlan = {
+		children: ControlPlan[]
+		expanded?: boolean
+		label: string
+		type: 'folder'
+	}
+
+	type ControlPlan = {
+		key: string
+		label: string
+		type: 'control'
+	}
+
+	type Plan = ControlPlan | FolderPlan
+
+	// Key string is the hash of the keys in the folder
+	type ExpandedState = Record<string, boolean>
+
+	// Defaults
 	const logPrefix = '[tweakpane-css]'
+	const defaultOptions: Options = {
+		autoFolders: false,
+		includeCalculated: false,
+		prettyNames: true,
+		showUnits: true,
+		sortNames: false,
+	}
+
+	// Stores
+
+	// Set up persistent local options store, with defaults
+	optionsStore = persisted('css-options', defaultOptions)
+
+	// Allow props to override
+	export let options: Options = $optionsStore
+
+	// Store folder states
+
+	expandedStateStore = persisted('css-expanded-state', {})
 
 	// Helper functions
 	function parseNumberOrReturnOriginal(text: string): number | string {
 		// Also strips suffixed units
 		const parsed = Number.parseFloat(text)
 		return Number.isNaN(parsed) ? text : parsed
+	}
+
+	function getHash(children: ControlPlan[]): string {
+		const rawKeyString = children.map((child) => child.key).join('')
+		// TODO Hash the key string
+		return rawKeyString
 	}
 
 	async function copyToClipboard(text: string): Promise<void> {
@@ -54,15 +115,96 @@
 		}
 	}
 
+	function stripLabelPrefix<T extends Plan>(plan: T): T {
+		return {
+			...plan,
+			label: stripPrefix(plan.label),
+		}
+	}
+
+	function stripPrefix(name: string): string {
+		return name.split(' ').slice(1).join(' ')
+	}
+
+	function getControlPlanFromStore(
+		cssVariableKeys: string[] | undefined,
+		options: Options,
+	): Plan[] {
+		if (cssVariableKeys === undefined) return []
+		// Sort if needed
+		const keys = options.sortNames ? cssVariableKeys.toSorted() : cssVariableKeys
+
+		// First pass omits the unwanted
+		const controls = keys.reduce<ControlPlan[]>((accumulator, key) => {
+			const originalValue = window.getComputedStyle(document.documentElement).getPropertyValue(key)
+
+			if (!options.includeCalculated && originalValue.includes('calc(')) {
+				return accumulator
+			}
+
+			const units = getUnits(originalValue)
+			return [
+				...accumulator,
+				{
+					key,
+					label: `${options.prettyNames ? cleanName(key) : key}${units && options.showUnits ? ` (${units})` : ''}`,
+					type: 'control',
+				},
+			]
+		}, [])
+
+		// Second pass to group into folders, maintaining order
+		if (options.autoFolders && controls.length > 1) {
+			const autoFolderControls: Plan[] = []
+
+			for (const [index, control] of controls.entries()) {
+				const lastPrefix = index > 0 ? cleanName(controls[index - 1].key).split(' ')[0] : undefined
+				const thisPrefix = cleanName(control.key).split(' ')[0]
+				const nextPrefix =
+					index < controls.length - 1 ? cleanName(controls[index + 1].key).split(' ')[0] : undefined
+
+				if ((lastPrefix === undefined || lastPrefix !== thisPrefix) && thisPrefix === nextPrefix) {
+					// Start folder
+					autoFolderControls.push({
+						children: [options.prettyNames ? stripLabelPrefix(control) : control],
+						expanded: false,
+						label: thisPrefix,
+						type: 'folder',
+					})
+				} else if (lastPrefix === thisPrefix) {
+					// Add to folder
+					const lastFolder = autoFolderControls.at(-1) as FolderPlan
+					lastFolder.children.push(options.prettyNames ? stripLabelPrefix(control) : control)
+				} else {
+					// Push at top level
+					autoFolderControls.push(control)
+				}
+			}
+
+			return autoFolderControls
+		}
+
+		return controls
+	}
+
+	function updatePlanForStore(cssVariableKeys: string[] | undefined, options: Options) {
+		controlPlan = []
+
+		// Some horrible thing is messing up the order of the controls after
+		// some are removed from a folder... this fixes it
+		tick()
+			.then(() => {
+				controlPlan = getControlPlanFromStore(cssVariableKeys, options)
+			})
+			.catch(console.error)
+	}
+
 	function cleanName(name: string): string {
 		return name
 			.replace('--', '')
 			.replaceAll('-', ' ')
 			.replaceAll(/\w\S*/g, (text) => text.charAt(0).toUpperCase() + text.slice(1).toLowerCase())
 	}
-
-	// Set up stores for local persistence
-	let cssVariableStore: Writable<Record<string, number | string>>
 
 	onMount(() => {
 		// Get all the root css variables
@@ -105,7 +247,7 @@
 			}
 
 			case 'Reset': {
-				reset()
+				resetCssVariables()
 				break
 			}
 
@@ -126,11 +268,32 @@
 		void copyToClipboard(`:root {\n${directives.join('')}}`)
 	}
 
-	function reset() {
+	function resetCssVariables() {
 		console.log(`${logPrefix} Clearing changes to CSS Variables`)
 		if (typeof localStorage !== 'undefined') {
 			localStorage.removeItem('css')
 			location.reload()
+		}
+	}
+
+	function resetOptions() {
+		for (const key of Object.keys($expandedStateStore)) {
+			$expandedStateStore[key] = true
+		}
+
+		$optionsStore = defaultOptions
+	}
+
+	function arraysEqual<T>(a: T[], b: T[]): boolean {
+		return a.length === b.length && a.every((value, index) => value === b[index])
+	}
+
+	function updateCssVariableKeys(store: Record<string, number | string>) {
+		if (!store) return
+		const latestKeys = Object.keys(store)
+
+		if (!arraysEqual(latestKeys, cssVariableKeys)) {
+			cssVariableKeys = latestKeys
 		}
 	}
 
@@ -148,21 +311,35 @@
 			)
 		}
 	}
+
+	let controlPlan: Plan[] = []
+	let cssVariableKeys: string[] = []
+
+	$: $optionsStore = options
+	$: updateCssVariableKeys($cssVariableStore)
+	$: updatePlanForStore(cssVariableKeys, $optionsStore)
 </script>
 
-<Pane localStoreId="tweakpane-css" title="Tweakpane CSS">
+<Pane localStoreId="tweakpane-css" position={'draggable'} title="Tweakpane CSS">
 	{#if cssVariableStore}
-		{#each Object.keys($cssVariableStore) as variableName}
-			{@const originalValue = window
-				.getComputedStyle(document.documentElement)
-				.getPropertyValue(variableName)}
-			{@const units = getUnits(originalValue)}
-			<AutoValue
-				bind:value={$cssVariableStore[variableName]}
-				label={`${cleanName(variableName)}${units ? ` (${units})` : ''}`}
-			/>
+		{#each controlPlan as plan}
+			{#if plan.type === 'folder'}
+				<Folder bind:expanded={$expandedStateStore[getHash(plan.children)]} title={plan.label}>
+					{#each plan.children as child}
+						{#if child.type === 'control'}
+							<AutoValue bind:value={$cssVariableStore[child.key]} label={child.label} />
+						{/if}
+					{/each}
+				</Folder>
+			{:else if plan.type === 'control'}
+				<AutoValue bind:value={$cssVariableStore[plan.key]} label={plan.label} />
+			{/if}
 		{/each}
+		<Separator />
+		<ButtonGrid buttons={['Copy', 'Reset']} on:click={handleClick} />
+		<Folder expanded={false} title="Tweakpane CSS Options">
+			<AutoObject bind:object={$optionsStore} />
+			<Button on:click={resetOptions} title="Reset Options" />
+		</Folder>
 	{/if}
-	<Separator />
-	<ButtonGrid buttons={['Copy', 'Reset']} on:click={handleClick} />
 </Pane>
